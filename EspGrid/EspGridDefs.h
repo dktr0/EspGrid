25a1ab262f5a00031897167e538ecf9b420a6599
David Ogborn <ogbornd@mcmaster.ca>
2016-01-02 16:48:29 -0500

    cosmetic changes after last commit

--- EspGrid/EspGridDefs.h
+++ EspGrid/EspGridDefs.h
@@ -98,11 +98,8 @@ inline static EspTimeType monotonicTime(void) {
 #else
 #ifdef _WIN32
 	// Windows (MINGW/GNUSTEP)
-	// LARGE_INTEGER frequency;
   extern LARGE_INTEGER performanceFrequency;
 	LARGE_INTEGER t;
-	// QueryPerformanceFrequency(&performanceFrequency);
-	// note: apparently function above is slow so we should probably do this differently
 	QueryPerformanceCounter(&t);
 	EspTimeType x = t.QuadPart / performanceFrequency.QuadPart * 1000000000L; // whole seconds in nanoseconds
 	EspTimeType y = t.QuadPart % performanceFrequency.QuadPart * 1000000000L / performanceFrequency.QuadPart; // remainder
